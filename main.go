da487a6eda7dbdaf70a0cb0e0b53f347c478ab5d
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2023-08-31 14:38:01 +0530

    Blank use of azure resources/storage package

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+    _ "github.com/pulumi/pulumi-azure-native-sdk/resources"
+    _ "github.com/pulumi/pulumi-azure-native-sdk/storage"
+)
+func main() {
+    fmt.Println("Hello")
+}
